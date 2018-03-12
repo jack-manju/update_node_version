223eea435542ad59026b97bf838cde3dd149dc9d
Gaurav Parmar <gaurav.parmar@isobar.com>
2018-03-12 11:49:52 +0530

    version updated

--- update_version.sh
+++ update_version.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-repos=( "git@github.com:parmar-gaurav/update_node_version.git" 
-        "git@github.com:parmar-gaurav/update_node_version.git" )
+repos=( "https://github.com/parmar-gaurav/nodejs.git" 
+        "https://github.com/parmar-gaurav/test_repo.git" )
 
 for i in "${repos[@]}"
 do 
    NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 7 | head -n 1) 
-ssh -i "$GIT_SSH_KEYS_PATH" git clone -b  development --single-branch $i  clone-git-repo/clone_$NEW_UUID
+   git clone -b  development --single-branch $i clone_$NEW_UUID
  done
 
+#apt-get install npm
+#npm install
+#npm install -g grunt-cli
+#grunt bump -v
 
-apt-get install npm
-npm install
-npm install -g grunt-cli
+#mv commit.sh clone-git-repo/
+#cd clone-git-repo/
+#chmod +x commit.sh
+#./commit.sh
 
-grunt bump -v
-
-mv commit.sh clone-git-repo/
-cd clone-git-repo/
-chmod +x commit.sh
-./commit.sh
+for i in `ls|grep clone`; 
+do 
+cd clone_$i
+npm version patch
+echo "--- Pushing clone_$i"; #cd $i; 
+ git commit -am 'version updated' && 
+ git push; cd ..; 
+echo "--- Finished $i"; 
+done
